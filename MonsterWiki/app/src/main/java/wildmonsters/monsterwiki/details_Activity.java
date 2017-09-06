aef56baf90e550b7ad4d3fa588025f057124ea16
yosh970 <josh.jmas@gmail.com>
2017-09-06 19:38:45 +0200

    Activity details avec linear layout et scroll view

--- MonsterWiki/app/src/main/java/wildmonsters/monsterwiki/details_Activity.java
+++ MonsterWiki/app/src/main/java/wildmonsters/monsterwiki/details_Activity.java
@@ -8,6 +8,6 @@ public class details_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details_);
+        setContentView(R.layout.activity_details2);
     }
 }
